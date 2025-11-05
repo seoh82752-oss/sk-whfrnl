2aae4c4d670d90fce8ff2b3ad14f7a72d18c1b33
seoh82752-oss <seoh82752@gmail.com>
2025-11-05 09:24:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ st.title('프린세스 메이커')
 a=st.text_input('부부가 공에 올라가면?')
 b=st.selectbox('닭이 옷사러 가서 하는말은?',['꼭끼오'])
 if st.button('인사말 생성'):
-  st.write(a+'쁑')
-  st.write(b+콬ㅎㅋㅎㅋㅎㅋㅋㅋㅋ)
+  st.info(a+'쁑')
+  st.warning(b+'콬ㅎㅋㅎㅋㅎㅋㅋㅋㅋ')
+  st.error('뿡ㅋㅋㅋㅋㅋㅋㅋ')
