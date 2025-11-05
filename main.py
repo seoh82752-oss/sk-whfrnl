d5d71f3c4b72078519609c7f394479b32ea19982
seoh82752-oss <seoh82752@gmail.com>
2025-11-05 09:18:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('프린세스 메이커')
 a=st.text_input('부부가 공에 올라가면?')
-st.selectbox('닭이 옷사러 가서 하는말은?',['꼭끼오']
+st.selectbox('닭이 옷사러 가서 하는말은?',['꼭끼오'])
 if st.button('인사말 생성'):
   st.write(a+'쁑')
